e51da4816996fdbcfc13e325896143d4242f6540
Ilya Tyupo <derherrmannelig@gmail.com>
2023-11-14 16:44:42 +0200

    feat: add pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
+  projectId: 'b4zuym',
   e2e: {
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
