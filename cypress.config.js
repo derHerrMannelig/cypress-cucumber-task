5a0874dfba1bc79356bceb98f3fc99c7d32b3bde
Ilya Tyupo <derherrmannelig@gmail.com>
2023-11-14 17:26:31 +0200

    feat: add html reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,13 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
   projectId: 'b4zuym',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: "https://www.saucedemo.com",
-    supportFile: false,
     specPattern: "./cypress/e2e/features/*.feature",
     chromeWebSecurity: false
   },
--- mobile.config.js
+++ mobile.config.js
@@ -3,12 +3,13 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
   projectId: 'b4zuym',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: "https://www.saucedemo.com",
-    supportFile: false,
     specPattern: "./cypress/e2e/features/*.feature",
     chromeWebSecurity: false,
     userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
